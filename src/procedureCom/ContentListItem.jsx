ffc8e684cb78500a290af6830d78d11ecae47a8e
masakiAndMarron <masakiAndMarron@users.noreply.github.com>
2022-05-08 23:30:35 +0900

    update RightContainer -v3.2.4

--- src/procedureCom/ContentListItem.jsx
+++ src/procedureCom/ContentListItem.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import DeleteForeverTwoToneIcon from "@mui/icons-material/DeleteForeverTwoTone";
+import { Divider } from "@mui/material";
 
 const ContentListItem = (props) => {
   return (
     <>
-      <li>
-        <div>{props.contents}</div>
-        <div>
+      <li className="content-container">
+        <div className="content">{props.contents}</div>
+        <div className="content-delete">
           <DeleteForeverTwoToneIcon />
         </div>
       </li>
+      <Divider variant="inset" component="li" />
     </>
   );
 };
