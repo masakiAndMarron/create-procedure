ff66a0e14625589f58c344259e37683d567873c2
masakiAndMarron <masakiAndMarron@users.noreply.github.com>
2022-11-17 23:45:50 +0900

    update

--- src/procedureCom/PhaseListItem.jsx
+++ src/procedureCom/PhaseListItem.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ContentListItem from "./ContentListItem";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { useState } from "react";
 
 const PhaseListItem = (props) => {
+  const [contentVisible, setContentVisible] = useState(true);
   const procedureType = props.procedureType;
   const setTitle = props.setTitle;
   const setClump = props.setClump;
@@ -11,28 +15,55 @@ const PhaseListItem = (props) => {
   const clumpId = tempProcedure[phase].id;
   const contentList = props.clumps[procedureType][phase];
 
+  const onContentVisible = () => {
+    setContentVisible(true);
+  };
+
+  const offContentVisible = () => {
+    setContentVisible(false);
+  };
+
   return (
     <>
       <ol className="phase-container">
         <div className="phase-name-wrapper">
           <div className="phase-name">
+            <div className="open-icon">
+              {contentVisible ? (
+                <KeyboardArrowDownIcon
+                  onClick={() => {
+                    offContentVisible();
+                  }}
+                />
+              ) : (
+                <KeyboardArrowRightIcon
+                  onClick={() => {
+                    onContentVisible();
+                  }}
+                />
+              )}
+            </div>
             <h3>{phase}</h3>
           </div>
         </div>
-        {tempProcedure[phase].contents &&
-          tempProcedure[phase].contents.map((value, index) => (
-            <ContentListItem
-              procedureType={procedureType}
-              setTitle={setTitle}
-              titleId={titleId}
-              setClump={setClump}
-              clumpId={clumpId}
-              content={value.content}
-              id={value.id}
-              contentList={contentList}
-              key={value.id}
-            />
-          ))}
+        {contentVisible && (
+          <div className="content-list-wrapper">
+            {tempProcedure[phase].contents &&
+              tempProcedure[phase].contents.map((value, index) => (
+                <ContentListItem
+                  procedureType={procedureType}
+                  setTitle={setTitle}
+                  titleId={titleId}
+                  setClump={setClump}
+                  clumpId={clumpId}
+                  content={value.content}
+                  id={value.id}
+                  contentList={contentList}
+                  key={value.id}
+                />
+              ))}
+          </div>
+        )}
       </ol>
     </>
   );
